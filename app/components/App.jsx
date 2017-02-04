70b8c775ca93b87e0d648514daa58a8055da9a10
rsstdd <rosstod@gmail.com>
2017-02-03 19:17:32 -0800

    Troubleshooting routes/auth

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -87,7 +87,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    console.log('App: UserId --> ', this.state.userId);
+    // console.log('App: UserId --> ', this.state.userId);
     console.log('App: favs --> ', this.state.favorites);
     return (
       <BrowserRouter>
